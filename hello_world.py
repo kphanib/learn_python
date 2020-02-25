417cc3cd206ae042cab8e8674cc404f5e88d54a7
Phani K <phani@Phanis-MacBook-Pro.local>
2020-02-25 15:38:31 -0500

    learned string interpolation

--- hello_world.py
+++ hello_world.py
@@ -14,4 +14,14 @@ print(a.lower())
 print(a.upper())
 print(id(a))
 #object a is assigned within a function example: len(a)
-#object a is assigned to a method example: a.upper
+#object a is assigned to a method example: a.upper()
+word_to_test = "level"
+print(word_to_test[::-1])
+my_daily_thought = "I am the first half I am the second half"
+print(len(my_daily_thought)//2)
+print(my_daily_thought[20:])
+my_string = "There's a lot going on in the universe don't you think?"
+print(len(my_string[::2]))
+
+#clean format usage | string interpolation
+print("hello {}".format("world"))
