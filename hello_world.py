b5628b3b64935c6e01cd85d15a2b9441ecc08f26
Phani K <phani@phanis-mbp.myfiosgateway.com>
2020-02-24 22:14:43 -0500

    learnt methods and functions

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,17 @@
 print("hello world")
+#slicing
+a = "interstellar"
+print(a[0:5]) #inter
+print(a[5:]) #stellar
+#stepsize 
+print(a[2:5:2])
+#methods and functions that can be used on string objects
+"""len() type() id() capitalize() upper() lower() strip() find() split() join()"""
+print(len(a))
+print(type(a))
+print(a.capitalize())
+print(a.lower())
+print(a.upper())
+print(id(a))
+#object a is assigned within a function example: len(a)
+#object a is assigned to a method example: a.upper
