d6b2a28627a9ed5f435b0b74dcf860269fd0dce2
Phani K <phani@Phanis-MacBook-Pro.local>
2020-02-26 15:39:18 -0500

    exersice and solutions

--- exercise-1-solutions.py
+++ exercise-1-solutions.py
@@ -1,18 +1,22 @@
 # Welcome to the assessment test for Python basics.
 
 # Question 1: Given the 4 numbers below, fill in the code
-# for the 8 lines below to provide the appropriate output
+# for the lines below where it says **fill in the code here**
+# to provide the appropriate output
 num_1 = 10
 num_2 = 300
 num_3 = 27.0
 num_4 = -10
 print(f"Calculations section:\nnum_1-> {num_1}\nnum_2-> {num_2}\nnum_3-> {num_3}\nnum_4-> {num_4}")
-print(f"{num_1} multiplied by {num_2} is: {num_1*num_2}")
+print(f"{num_1} multiplied by {num_2} is: {num_1 * num_2}")
 print(f"{num_3} divided by {num_1} is: {num_3/num_1}")
 print(f"If I use floor division instead I get: {num_3//num_1}")
 print(f"The absolute value of {num_4} is: {abs(num_4)}")
 print(f"I converted {num_1} and {num_2} to strings, if I add them I get: {str(num_1)+str(num_2)}")
-print(f"{num_4} is of the **fill in the code here** class")
+print(f"{num_4} is of the {type(num_4)} class")
+# (Advanced) To show just the type instead of <class 'int'>, uncomment
+# the line below:
+# print(f"{num_4} is of the {type(num_4).__name__} class")
 print("-"*40)
 print()
 
@@ -23,9 +27,9 @@ print()
 name_of_job = "Price updater"
 begin_time = "17:30:00"
 end_time = "18:30:00"
-print(f"Added:\t\t {name_of_job}")
-print(f"Begin:\t\t {begin_time}")
-print(f"End:\t\t {end_time}")
+print(f"Added:\t\t{name_of_job}")
+print(f"Begin:\t\t{begin_time}")
+print(f"End:\t\t{end_time}")
 print("-"*40)
 print()
 
@@ -34,29 +38,27 @@ print()
 # Example output-> Added:     Price updater
 #                  Begin:     17:30:00
 #                  End:       18:30:00
-print(f"Added:\t\t {name_of_job} \nBegin:\t\t {begin_time} \nEnd:\t\t {end_time}")
+print(f"Added:\t\t{name_of_job}\nBegin:\t\t{begin_time}\nEnd:\t\t{end_time}")
 print("-"*40)
 print()
 
 # Question 3: Use the .format method to update the line below
-print("{0} scheduled to begin at {1} and end at {2}".format(name_of_job, begin_time, end_time))
+print("{} scheduled to begin at {} and end at {}".format(name_of_job, begin_time, end_time))
 print("-"*40)
 print()
 
 # Question 4: Use the pow function from math module to evaluate 2 to the
 # power of 5, or 2^5
 import math
-from math import pow
 print("2 to the power of 5 is listed below...")
-print(f"{math.pow(2,5)}")
+print(math.pow(2,5))
 print("-"*40)
 print()
 
 # Question 5: Use the log2 function from math module to evalute log base 2
 # of 32
-from math import log2
 print("log base 2 of 32 is listed below...")
-print(f"{math.log2(32)}")
+print(math.log2(32))
 print("-"*40)
 print()
 
@@ -65,5 +67,4 @@ print()
 # Hint: You will need to use a function to find the length first and then use that,
 # in combination with the number to come up with the start index
 welcome_message = "Hello and welcome to the land of Python"
-print(f"length of welcome_message is {len(welcome_message)}")
-print(f"The last 6 letters of the welcome message:\n'{welcome_message}'\nare: '{welcome_message[len(welcome_message)-6:]}'")
+print(f"The last 6 letters of the welcome message:\n'{welcome_message}'\nare: {welcome_message[len(welcome_message)-6:]}")
